7c9953cbea8021f041c89f3347edf69c52f7670c
Zakria Yousafzai <zakriakhanz2003@gmail.com>
2024-08-08 20:54:21 +0500

    Add files via upload

---
+++ Flip-A-Coin/script.js
@@ -0,0 +1,44 @@
+const coin = document.querySelector(".coin");
+const flipBtn = document.querySelector("#flip-button");
+const resetBtn = document.querySelector("#reset-button");
+let heads = 0;
+let tails = 0;
+
+flipBtn.addEventListener("click", () => {
+    let i = Math.floor(1 + Math.random() * 2);
+    coin.style.animation = "none";
+    
+    if(i == 1){
+        setTimeout(function(){
+            coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+        heads++;
+    }
+    else{
+        setTimeout(function(){
+            coin.style.animation = "spin-tails 3s forwards";
+        }, 100);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
+
+function updateStats(){
+    document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+    document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+}
+
+function disableButton(){
+    flipBtn.disabled = true;
+    setTimeout(function(){
+        flipBtn.disabled = false;
+    },3000);
+}
+
+resetBtn.addEventListener("click",() => {
+    coin.style.animation = "none";
+    heads = 0;
+    tails = 0;
+    updateStats();
+});
